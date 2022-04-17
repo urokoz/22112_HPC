6989f7008f9d9cdeabe2bd63e0b057db45e4f20a
urokoz <urokoz@gmail.com>
2022-04-18 01:06:57 +0200

    Updated ex 10 for handin

--- Lec10/ex10_1.py
+++ Lec10/ex10_1.py
@@ -6,21 +6,40 @@ import time
 import itertools
 import numpy as np
 
+start_time = time.time()
+
 
 def overrep_kmer(infile_name, nt_dict, kmer_dict, pos, k):
+    """ Count the occurences of each possible 5mer and count the occurences of
+        each nucleotide in a sequence.
 
+        Inputs:
+        infile_name         File containing fasta sequence
+        nt_dict             Dictionary containing the occurences of nucleotides
+        kmer_dict           Dictionary containing the occurences of kmers
+        pos                 Vector containing the indexes of the sequence
+        k                   Size of the kmer
+
+        Outputs:
+        nt_dict             Dictionary containing the occurences of nucleotides
+        kmer_dict           Dictionary containing the occurences of kmers
+    """
+    # open file, extract sequence and remove newlines
     with open(infile_name, "rb") as infile:
         infile.seek(pos[2])
         seq = infile.read(pos[3] - pos[2])
-
     seq = seq.translate(None, delete=b"\n")
 
+    # add the occurences of the 4 nucleotides to the values already in the dict
     for nt in b"atcg":
         nt_dict[nt] += seq.count(nt)
 
+    # count the occurences of the possible kmers
     for i in range(len(seq)-(k-1)):
-        kmer = seq[i:i+k]
+        kmer = seq[i:i+k]   #extract sequence
 
+        # add kmer occurence to the kmer dict. Add an entry if the entry doesn't
+        # already exist
         try:
             kmer_dict[kmer] += 1
         except:
@@ -28,13 +47,14 @@ def overrep_kmer(infile_name, nt_dict, kmer_dict, pos, k):
 
     return nt_dict, kmer_dict
 
-
+# check input and extract commandline arguments
 if len(sys.argv) != 3:
     sys.exit("Usage: ex10_1.py <input fasta file> <k>")
 
 filename = sys.argv[1]
 k = int(sys.argv[2])
-# open files with byteread
+
+# open file with byteread
 try:
     infile = open(filename, "rb")
 except IOError as err:
@@ -90,33 +110,51 @@ index_list.append([headerstart, headerend, seq_start, seqend])
 
 infile.close()
 
+# initiate dicts
 kmer_dict = dict()
 nt_dict = dict()
 for nt in b"atcg":
     nt_dict[nt] = 0
 
+setup_end_time = time.time()
 
+# count the occurences of the kmers and nucleotides for each entry in the fasta
 for i, pos in enumerate(index_list):
-    print("Working on sequence {}".format(i+1))
+    print("# Working on sequence {}".format(i+1))
     nt_dict, kmer_dict = overrep_kmer(filename, nt_dict, kmer_dict, pos, k)
 
+# filter out any non atcg kmers
 filtered_kmer_dict = dict()
 for kmer_list in itertools.product("atcg", repeat=k):
     kmer = "".join(kmer_list).encode()
-    filtered_kmer_dict[kmer] = kmer_dict[kmer]
+    filtered_kmer_dict[kmer] = kmer_dict.get(kmer, 0)
 
+occurence_count_time = time.time()
+
+# calculate total amount of nucleotides and kmers
 tot_nt = sum(nt_dict.values())
 tot_kmer = sum(filtered_kmer_dict.values())
 
+# calculate if kmers are overrepresented and add overrepresented kmers to a list
 overreps = []
 for kmer, count in filtered_kmer_dict.items():
-    random_chance = np.prod([nt_dict[nt]/tot_nt for nt in kmer])
+    # calculate the expected occurence of a given kmer
+    expected_rep = np.prod([nt_dict[nt]/tot_nt for nt in kmer])
 
-    actual_rep = count/tot_kmer
-    if actual_rep > (2*random_chance):
-        overreps.append((kmer, actual_rep/random_chance))
+    actual_rep = count/tot_kmer     # calculate the actual occurence
+    if actual_rep > (2*expected_rep):   # add overrepresented kmers to list
+        overreps.append((kmer, actual_rep/expected_rep))
 
-overreps = sorted(overreps, key=lambda x:x[1], reverse = True)
+overreps = sorted(overreps, key=lambda x:x[1], reverse = True)  # sort by overrepresention
 
+overreps_time = time.time()
+
+# print overrepresented kmers and their overrepresention
 for kmer, rep in overreps:
-    print(kmer.decode(), rep, sep="\t")
+    print(kmer.decode(), round(rep, 3), sep="\t")
+
+                                                                                            # time for 7mer
+print("# Indexing and setup:", round(setup_end_time - start_time, 5))                       # 0.45559 s
+print("# kmer and nucleotide count:", round(occurence_count_time - setup_end_time, 5))      # 768.54909 s
+print("# Overrepresention:", round(overreps_time - occurence_count_time, 5))                # 0.09963 s
+print("# Total:", round(overreps_time - start_time, 5))                                     # 769.10431 s
--- Lec10/ex10_2.py
+++ Lec10/ex10_2.py
@@ -9,56 +9,82 @@ import pickle
 
 start_time = time.time()
 
-def overrep_kmer(infile_name, global_dict_path, pos, k, job_nr):
-    print("Working on {}".format(job_nr))
 
+def overrep_kmer(infile_name, global_dict_path, pos, k, job_nr):
+    """ Count the occurences of each possible 5mer and count the occurences of
+        each nucleotide in a sequence. Safely save the kmer dict to a file that
+        is accessible to other jobs.
+
+        Inputs:
+        infile_name         File containing fasta sequence
+        pos                 Vector containing the indexes of the sequence
+        k                   Size of the kmer
+
+        Outputs:
+        nt_dict             Dictionary containing the occurences of nucleotides
+    """
+    print("# Working on {}".format(job_nr))
+
+    # open file, extract sequence and remove newlines
     with open(infile_name, "rb") as infile:
         infile.seek(pos[2])
         seq = infile.read(pos[3] - pos[2])
-
     seq = seq.translate(None, delete=b"\n")
 
+    # add the occurences of the 4 nucleotides
     nt_dict = dict()
     for nt in b"atcg":
         nt_dict[nt] = seq.count(nt)
-    
+
+    # count the occurences of the possible kmers
     kmer_dict = dict()
     for i in range(len(seq)-(k-1)):
-        kmer = seq[i:i+k]
+        kmer = seq[i:i+k]   #extract sequence
 
+        # add kmer occurence to the kmer dict. Add an entry if the entry doesn't
+        # already exist
         try:
             kmer_dict[kmer] += 1
         except:
             kmer_dict[kmer] = 1
 
-
-    # semaphore-based memory efficient global kmer count
+    # semaphore-based memory efficient saving of kmer occurence to a file
+    # accessible to other jobs. This prevents having to store a large dict for
+    # each job by adding the kmer occurences for this job to a global dict.
+    # creating and removing a directory is used as a semaphore to prevent errors
+    # from two jobs adding to the dict at the same time.
     while True:
         try:
-            os.mkdir("/tmp/ex_10_semaphore")
-            print("Worker {} created semaphore".format(job_nr))
+            # create semaphore
+            os.mkdir("/tmp/ex_10_semaphore")    #
+            print("# Worker {} created semaphore".format(job_nr))
 
+            # load global kmer dict
             with open(global_dict_path, "rb") as f:
                 global_kmer_dict = pickle.load(f)
 
+            # merge local and global kmer dicts. This also filters out non atcg
+            # kmers as the global dict only includes atcg kmers as keys.
             for kmer in global_kmer_dict.keys():
                 global_kmer_dict[kmer] += kmer_dict.get(kmer, 0)
 
+            # save global kmer dict
             with open(global_dict_path, "wb") as f:
                 pickle.dump(global_kmer_dict, f)
 
+            # release semaphore
             os.rmdir("/tmp/ex_10_semaphore")
-            print("Worker {} released semaphore".format(job_nr))
+            print("# Worker {} released semaphore".format(job_nr))
             break
 
-        except:
-            print("Worker {} sleeping waiting for semaphore".format(job_nr))
+        except:     # wait for semaphore to be free
+            print("# Worker {} sleeping waiting for semaphore".format(job_nr))
             time.sleep(0.1)
 
-    print("Finished {}".format(job_nr))
+    print("# Finished {}".format(job_nr))
     return nt_dict
 
-
+# check input and extract commandline arguments
 if len(sys.argv) != 3:
     sys.exit("Usage: ex10_2.py <input fasta file> <k>")
 
@@ -122,44 +148,69 @@ infile.close()
 # sort in order to load balance
 index_list = sorted(index_list, key=lambda x:(x[3]-x[2]), reverse = True)
 
-index_end_time = time.time()
 
+# initiate global kmer dict as containing all possible k long atcg kmers as
+# byte strings
 global_kmer_dict = dict()
 for kmer in itertools.product("atcg", repeat=k):
     global_kmer_dict["".join(kmer).encode()] = 0
 
+# save global kmer dict
 global_dict_path = "/tmp/global_kmer_dict.pkl"
-with open(global_dict_path, "wb") as f:
-    pickle.dump(global_kmer_dict, f)
-
 try:
+    with open(global_dict_path, "wb") as f:
+        pickle.dump(global_kmer_dict, f)
+except IOError as err:
+    sys.exit("Error saving global kmer dictionary: " + str(err))
+
+# make sure semaphore works
+try:
+    os.mkdir("/tmp/ex_10_semaphore")
     os.rmdir("/tmp/ex_10_semaphore")
-except:
-    pass
+except IOError as err:
+    sys.exit("Error creating semaphore: " + str(err))
 
-dir_created_time = time.time()
+setup_end_time = time.time()
 
+# count the occurences of the kmers and nucleotides for each entry in the fasta
+# using parallization
 nt_dicts = jl.Parallel(n_jobs=8)(jl.delayed(overrep_kmer)(filename, global_dict_path, pos, k, job_nr+1) for job_nr, pos in enumerate(index_list))
 
+occurence_count_time = time.time()
+
+# collect nucleotide occurences to one dict
 total_nt_dict = dict()
 for nt in b"atcg":
     total_nt_dict[nt] = sum([dict.get(nt, 0) for dict in nt_dicts])
 
+# load global kmer dict
 with open(global_dict_path, "rb") as f:
     global_kmer_dict = pickle.load(f)
 
+# calculate total amount of nucleotides and kmers
 tot_nt = sum(total_nt_dict.values())
 tot_kmer = sum(global_kmer_dict.values())
 
+# calculate if kmers are overrepresented and add overrepresented kmers to a list
 overreps = []
 for kmer, count in global_kmer_dict.items():
-    random_chance = np.prod([total_nt_dict[nt]/tot_nt for nt in kmer])
+    # calculate the expected occurence of a given kmer
+    expected_rep = np.prod([total_nt_dict[nt]/tot_nt for nt in kmer])
 
-    actual_rep = count/tot_kmer
-    if actual_rep > (2*random_chance):
-        overreps.append((kmer,actual_rep/random_chance))
+    actual_rep = count/tot_kmer     # calculate the actual occurence
+    if actual_rep > (2*expected_rep):   # add overrepresented kmers to list
+        overreps.append((kmer, actual_rep/expected_rep))
 
-overreps = sorted(overreps, key=lambda x:x[1], reverse = True)
+overreps = sorted(overreps, key=lambda x:x[1], reverse = True)  # sort by overrepresention
 
+overreps_time = time.time()
+
+# print overrepresented kmers and their overrepresention
 for kmer, rep in overreps:
-    print(kmer.decode(), rep, sep="\t")
+    print(kmer.decode(), round(rep, 3), sep="\t")
+
+                                                                                            # time for 7mer
+print("# Indexing and setup:", round(setup_end_time - start_time, 5))                       # 0.46656 s
+print("# kmer and nucleotide count:", round(occurence_count_time - setup_end_time, 5))      # 319.11068 s
+print("# Overrepresention:", round(overreps_time - occurence_count_time, 5))                # 0.10617 s
+print("# Total:", round(overreps_time - start_time, 5))                                     # 319.68341 s
